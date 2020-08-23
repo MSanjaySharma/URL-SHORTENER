82e783863c85b3ec70cb2f00e1c0472d7d1514d3
MSanjaySharma <sanjay98.sharma@gmail.com>
2020-08-23 20:31:06 +0530

    Added helmetJS

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 3333;
+const helmet = require("helmet");
 const useragent = require("express-useragent");
 
 const configureStore = require("./config/database");
 configureStore();
 
+app.use(helmet());
 app.use(express.json());
 app.use(useragent.express());
 
